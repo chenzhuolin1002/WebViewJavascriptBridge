435fd80746f275e44c832a454018d26c2e82b4d2
chenzhuolin <gzchenzhuolin@corp.netease.com>
2016-01-15 10:02:14 +0800

    修改delegate属性访问为weak

--- WebViewJavascriptBridge/WebViewJavascriptBridgeBase.h
+++ WebViewJavascriptBridge/WebViewJavascriptBridgeBase.h
@@ -21,7 +21,7 @@ typedef NSDictionary WVJBMessage;
 @interface WebViewJavascriptBridgeBase : NSObject
 
 
-@property (assign) id <WebViewJavascriptBridgeBaseDelegate> delegate;
+@property (weak) id <WebViewJavascriptBridgeBaseDelegate> delegate;
 @property (strong, nonatomic) NSMutableArray* startupMessageQueue;
 @property (strong, nonatomic) NSMutableDictionary* responseCallbacks;
 @property (strong, nonatomic) NSMutableDictionary* messageHandlers;
